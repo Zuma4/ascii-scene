0079155e3a5bd8c7c69ab0fe88fe71ed2505232d
Zuma4 <135827801+Zuma4@users.noreply.github.com>
2023-11-21 23:15:37 +0200

    Add files via upload

---
+++ ASCII_PONG_example.cpp
@@ -0,0 +1,61 @@
+#include "ASCII_SCENE.h"
+
+
+int main() {
+	Window window{AlphaDegree::Degree::black};
+	Shape player1{ 4, 10 };
+
+	Shape player2{ 4, 10 };
+	player2.setPosition({ term_cols - player2.getWidth(), 0 });
+
+	Shape ball{ 3, 2, {(term_cols / 2) - 2, 3}, AlphaDegree::Degree::white};
+	Position ball_movement = {3, -1};
+
+	Shape bottom{ term_cols, 1, {0, term_rows - 1}, AlphaDegree::Degree::white };
+	Shape top{ term_cols, 1, {0, 0}, AlphaDegree::Degree::white };
+
+	Shape colRight{ 1, term_rows, {0, 0}, AlphaDegree::Degree::low_white_grey };
+	Shape colLeft{ 1, term_rows, {term_cols - 1, 0}, AlphaDegree::Degree::low_white_grey };
+
+	while(true)
+	{
+		
+		ball.move(ball_movement);
+		
+		if (intersection(ball, bottom) || intersection(ball, top))
+			ball_movement.y *= -1;
+		if (intersection(ball, player1) || intersection(ball, player2))
+			ball_movement.x *= -1;
+
+		Shape* playerToMove;
+		if (ball_movement.x > 0)
+			playerToMove = &player2;
+		else
+			playerToMove = &player1;
+
+		if (ball.getPosition().y > playerToMove->getPosition().y + playerToMove->getHeight() / 2 &&
+			playerToMove->getPosition().y < term_rows - playerToMove->getHeight() - 1)
+			playerToMove->move({ 0, 1 });
+
+		else if (ball.getPosition().y < playerToMove->getPosition().y + playerToMove->getHeight() / 2 &&
+			playerToMove->getPosition().y > 1)
+			playerToMove->move({ 0, -1 });
+
+		window.clear();
+
+		window.draw(bottom);
+		window.draw(top);
+
+		window.draw(colLeft);
+		window.draw(colRight);
+
+		window.draw(player1);
+		window.draw(player2);
+
+		window.draw(ball);
+
+		window.display();
+	}
+
+	return 0;
+}
---
+++ ASCII_SCENE.h
@@ -0,0 +1,154 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <array>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+const int term_rows = 29;
+const int term_cols = 120;
+
+struct Position
+{
+	int x{};
+	int y{};
+};
+
+class AlphaDegree
+{
+public:
+	enum class Degree
+	{
+		black,
+		smooth_black,
+		black_grey,
+		medium_black_grey,
+		smooth_black_grey,
+		low_black_grey,
+		grey,
+		low_white_grey,
+		smooth_white_grey,
+		medium_white_grey,
+		white_grey,
+		smooth_white,
+		white,
+	};
+
+	AlphaDegree(Degree deg) : m_charDegree{ CharacterDegrees[static_cast<int>(deg)] } {}
+
+	operator char() const { return m_charDegree; }
+
+	static std::array<char, 13> CharacterDegrees;
+
+private:
+	char m_charDegree;
+};
+
+std::array<char, 13> AlphaDegree::CharacterDegrees{ ' ', '.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@' };
+
+
+class Shape
+{
+public:
+	Shape(int width = 0, int height = 0, Position position = { 0, 0 }, AlphaDegree::Degree color = AlphaDegree::Degree::white) :
+		m_width{ width }, m_height{ height }, m_position{ position }, m_degree{ color }
+	{}
+	virtual ~Shape() = default;
+	void move(const Position& direction)
+	{
+		m_position.x += direction.x;
+		m_position.y += direction.y;
+	};
+	void setPosition(const Position& direction)
+	{
+		m_position.x = direction.x;
+		m_position.y = direction.y;
+	};
+	void setColor(AlphaDegree::Degree degree) { m_degree = degree; };
+	const Position& getPosition() const { return m_position; };
+	const AlphaDegree& getColor() const { return m_degree; }
+	int getHeight() const { return m_height; };
+	int getWidth() const { return m_width; };
+
+private:
+	Position m_position;
+	AlphaDegree m_degree;
+	int m_height;
+	int m_width;
+};
+
+bool intersection(const Shape& shape1, const Shape& shape2)
+{
+	int start1_x = shape1.getPosition().x, start1_y = shape1.getPosition().y;
+	int end1_x = start1_x + shape1.getWidth(), end1_y = start1_y + shape1.getHeight();
+
+	int start2_x = shape2.getPosition().x, start2_y = shape2.getPosition().y;
+	int end2_x = start2_x + shape2.getWidth(), end2_y = start2_y + shape2.getHeight();
+
+	return((start1_x > start2_x && start1_x < end2_x) || (start2_x > start1_x && start2_x < end1_x)) &&
+		((start1_y > start2_y && start1_y < end2_y) || (start2_y > start1_y && start2_y < end1_y));
+
+}
+
+void cls()
+{
+	HANDLE hOut;
+	COORD Position;
+
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Position.X = 0;
+	Position.Y = 0;
+	SetConsoleCursorPosition(hOut, Position);
+}
+
+class Window {
+public:
+
+	Window(const AlphaDegree::Degree& degree = AlphaDegree::Degree::black) : m_degree{ degree }
+	{
+		clear();
+	}
+
+	void display() const
+	{
+		for (int y = 0; y < m_state.size(); ++y) {
+			for (int x = 0; x < m_state[0].size(); ++x)
+				std::cout << m_state[y][x];
+		}
+	}
+
+	void draw(const Shape& shape)
+	{
+		for (int h = shape.getPosition().y; h < shape.getHeight() + shape.getPosition().y; ++h)
+		{
+			if (h >= m_state.size())
+				break;
+
+			for (int w = shape.getPosition().x; w < shape.getWidth() + shape.getPosition().x; ++w) {
+				if (w >= m_state[0].size())
+					break;
+				m_state[h][w] = shape.getColor();
+			}
+		}
+	}
+
+	void clear()
+	{
+		cls();
+		for (int y = 0; y < term_rows; ++y) {
+			m_state[y].resize(term_cols);
+			for (int x = 0; x < term_cols; ++x)
+				m_state[y][x] = m_degree;
+			m_state[y].back() = '\n';
+		}
+	};
+
+	Window(const Window&) = delete;
+
+private:
+	std::array<std::string, term_rows> m_state;
+	AlphaDegree m_degree;
+};
+
